617e1741c19f9585b43b3a8c62da19d12fcf0321
antisankari <anssi.sankari@gmail.com>
2024-10-26 03:05:45 +0300

    added viewport component and basic API call to APP

---
+++ course-project/src/Viewport.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function Viewport() {
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <Card.Text>
+                        This is a test text
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default Viewport;
